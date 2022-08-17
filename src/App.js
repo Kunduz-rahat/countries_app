3eafa9765039015a462a55e2882ad17654dd5964
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-08-17 19:49:45 +0600

    add cardItem

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
-import React from "react";
+import axios from "axios";
+import React, {useState, useEffect} from "react";
 import { Controls } from "./components/Controls";
+import CountriesList from "./components/CountriesList";
+import CountryCard from "./components/CountryCard";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
+import { ALL_COUNTRIES } from "./config";
 
 
 
 function App() {
+  const [countries, setCountries] = useState([])
+console.log(countries)
+  useEffect(()=>{
+axios.get(ALL_COUNTRIES).then(
+  ({data}) => setCountries(data))
+  }, [])
   return (
     <>
       <Header/>
       <Main>
 <Controls/>
+<CountriesList>
+  {
+    countries.map(country =>{
+      const countryInfo={
+        img:country.flags.png,
+        name:country.name,
+        info:[
+          {
+            title: 'Population',
+            description: country.population.toLocaleString()
+          },
+          {
+            title: 'Region',
+            description: country.region
+          },
+          {
+            title: 'Capital',
+            description: country.capital
+          },
+        ],
+      };
+      return <CountryCard key={country.name} {...countryInfo}/>
+    })
+  }
+</CountriesList>
       </Main>
     </>
   )
