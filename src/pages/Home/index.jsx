4a37806c3940b5882e1af2c82cdb24528e671172
Kunduz Rahat <rahatova.kunduz@gmail.com>
2023-03-07 19:32:25 +0600

    add constanst

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,65 +1,67 @@
-import React, {useState, useEffect} from "react";
-import {useNavigate} from 'react-router-dom';
-import axios from "axios";
-import CountriesList from "../../components/CountriesList";
-import CountryCard from "../../components/CountryCard";
-import { Controls } from "../../components/Controls";
-import { ALL_COUNTRIES } from "../../config";
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import CountriesList from '../../components/CountriesList';
+import CountryCard from '../../components/CountryCard';
+import { Controls } from '../../components/Controls';
+import { ALL_COUNTRIES } from '../../config';
 
+export const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState(countries);
+  const navigate = useNavigate();
 
- export const Home = () => {
-	const [countries, setCountries] = useState([]);
-  const [filteredCountries, setFilteredCountries] = useState(countries)
-	const navigate = useNavigate()
-
-  const handleSearch =(search, region)=>{
-    let data=[...countries]
-    if(region){
-      data = data.filter(country=> country.region.includes(region))
+  const handleSearch = (search, region) => {
+    let data = [...countries];
+    if (region) {
+      data = data.filter((country) => country.region.includes(region));
     }
-    if(search){
-      data = data.filter(country=>country.name.toLowerCase().includes(search.toLowerCase()))
+    if (search) {
+      data = data.filter((country) =>
+        country.name.toLowerCase().includes(search.toLowerCase()),
+      );
     }
-    setFilteredCountries(data)
-  }
+    setFilteredCountries(data);
+  };
 
-	console.log(countries)
-	  useEffect(()=>{
-     if(!countries.length)
-	axios.get(ALL_COUNTRIES).then(
-	  ({data}) => setCountries(data))
-	  }, [])
-
+  console.log(countries);
+  useEffect(() => {
+    if (!countries.length)
+      axios.get(ALL_COUNTRIES).then(({ data }) => setCountries(data));
+  }, []);
 
   return (
-	 <>
-		<Controls onSearch={handleSearch}/>
-<CountriesList>
-  {
-    filteredCountries.map(country =>{
-      const countryInfo={
-        img:country.flags.png,
-        name:country.name,
-        info:[
-          {
-            title: 'Population',
-            description: country.population.toLocaleString()
-          },
-          {
-            title: 'Region',
-            description: country.region
-          },
-          {
-            title: 'Capital',
-            description: country.capital
-          },
-        ],
-      };
-      return <CountryCard key={country.name} onClick={()=> navigate(`/country/${country.name}`)} {...countryInfo}/>
-    })
-  }
-</CountriesList>
-	 </>
-  )
-}
-
+    <>
+      <Controls onSearch={handleSearch} />
+      <CountriesList>
+        {filteredCountries.map((country) => {
+          const countryInfo = {
+            img: country.flags.png,
+            name: country.name,
+            info: [
+              {
+                title: 'Population',
+                description: country.population.toLocaleString(),
+              },
+              {
+                title: 'Region',
+                description: country.region,
+              },
+              {
+                title: 'Capital',
+                description: country.capital,
+              },
+            ],
+          };
+          return (
+            <CountryCard
+              key={country.name}
+              onClick={() => navigate(`/country/${country.name}`)}
+              {...countryInfo}
+            />
+          );
+        })}
+      </CountriesList>
+    </>
+  );
+};
---
+++ src/store/reducers/coutriesList.js
@@ -0,0 +1,8 @@
+const initialState ={
+	countries:[],
+	loading:false
+}
+
+export const countriesReducer =(state = initialState, action)=>{
+
+}
